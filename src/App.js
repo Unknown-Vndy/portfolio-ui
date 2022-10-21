8d361f76eae1c7809c0b6ae4405d403bb17508b0
Andrey <budyko.andrey1@gmail.com>
2022-10-21 15:09:50 +0300

    add main page wrapper

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      qq
-    </div>
-  );
+function App () {
+  return <Wrapper></Wrapper>;
 }
 
 export default App;
+
+const Wrapper = styled.div`
+  max-width: 855px;
+  margin: 0 auto;
+`;
