37e441096f2c31a7f1c6894eb1d5b0c4e5e99584
Andrey <budyko.andrey1@gmail.com>
2022-10-23 21:45:23 +0300

    tried menu burger

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import { Route, Routes } from 'react-router-dom';
+import {
+  Route,
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+} from 'react-router-dom';
 import Blog from './components/Blog';
 import Contact from './components/Contact';
 import Layout from './components/Layout';
 import Works from './components/Works';
 import Home from './routes/Home';
 
-function App () {
-  return (
-    <Routes>
-      <Route exact path='/' element={<Layout />}>
-        <Route path='/' element={<Home />} />
-        <Route path='works' element={<Works />} />
-        <Route path='blog' element={<Blog />} />
-        <Route path='contact' element={<Contact />} />
-      </Route>
-    </Routes>
-  );
-}
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='/' element={<Home />} />
+      <Route path='works' element={<Works />} />
+      <Route path='blog' element={<Blog />} />
+      <Route path='contact' element={<Contact />} />
+    </Route>
+  )
+);
 
+function App () {
+  return <RouterProvider router={router} />;
+}
+// <Routes>
+//   <Route exact path='/' element={<Layout />}>
+// <Route path='/' element={<Home />} />
+// <Route path='works' element={<Works />} />
+// <Route path='blog' element={<Blog />} />
+// <Route path='contact' element={<Contact />} />
+//   </Route>
+// </Routes>
 export default App;
 
 // const Wrapper = styled.div`
--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,11 +1,32 @@
 import styled from 'styled-components';
 import NavigationLink from './NavigationLink';
-import { AiOutlineMenu } from 'react-icons/ai';
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
+import { useEffect, useState } from 'react';
+
+const Menu = ({ open, setIsOpen }) => {
+  if (open)
+    return (
+      <>
+        <NavigationLink to={'/works'} body={`Works`} />
+        <NavigationLink to={'/blog'} body={`Blog`} />
+        <NavigationLink to={'/contact'} body={`Contact`} />
+      </>
+    );
+  else return;
+};
 
 function NavMenu () {
+  const [open, setIsOpen] = useState(false);
+  useEffect(() => {
+    console.log('open :>> ', open);
+  }, [open]);
   return (
     <MenuContainer>
-      <Burger />
+      <StyledMenu>
+        <Burger onClick={() => setIsOpen(!open)} />
+        <Menu open={open} />
+      </StyledMenu>
+
       <Links>
         <NavigationLink to={'/works'} body={`Works`} />
         <NavigationLink to={'/blog'} body={`Blog`} />
@@ -16,6 +37,45 @@ function NavMenu () {
 }
 
 export default NavMenu;
+
+const StyledMenu = styled.nav`
+  position: absolute;
+  z-index: 10;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  background: #effffa;
+  
+  /* @media (max-width: 576px) {
+    width: 100%;
+    height: 100vh;
+  } */
+
+  /* transform: ${({ open }) => (open ? 'translateX(0)' : 'translateX(-100%)')};
+  height: 100vh;
+  text-align: left;
+  padding: 2rem;
+  transition: transform 0.3s ease-in-out; */
+
+  /* @media (max-width: 576px) {
+    width: 100%;
+  }
+  a {
+    font-size: 2rem;
+    text-transform: uppercase;
+    padding: 2rem 0;
+    font-weight: bold;
+    letter-spacing: 0.5rem;
+    color: #0d0c1d;
+    text-decoration: none;
+    transition: color 0.3s linear;
+
+    @media (max-width: 576px) {
+      font-size: 1.5rem;
+      text-align: center;
+    }
+  } */
+`;
 
 const MenuContainer = styled.div`
   max-width: 855px;
@@ -46,4 +106,9 @@ const Burger = styled(AiOutlineMenu)`
   @media (min-width: 800px) {
     display: none;
   }
+`;
+
+const Close = styled(AiOutlineClose)`
+  font-size: 2rem;
+  cursor: pointer;
 `;
